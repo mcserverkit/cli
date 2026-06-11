7447347fec0310c5c4017a602b3e69ca59e710db
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-11 13:20:40 -0700

    Add operating system detection

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
 echo "Installing..."
+
+# https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+echo "You're on Linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+echo "You're on macOS"
+else
+echo ""
+fi
+
